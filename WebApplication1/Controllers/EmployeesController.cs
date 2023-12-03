ba3738bdd2944ea61c388fdf134187555141dd62
Raghuveery <raghuveery@nvidia.com>
2023-12-03 22:58:09 +0530

    Test commit

---
+++ WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() {
+            Id = 1,
+            Name = "Test",
+            Address ="Address "
+            });
+            employees.Add(new Employee()
+            {
+                Id = 2,
+                Name = "Raghuveer ",
+                Address = "Lucknow"
+            });
+
+             return Ok(employees);
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmployeesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
